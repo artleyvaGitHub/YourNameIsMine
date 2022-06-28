e377d889e3b6e6542b4d2fae1d65967127f80f7c
Arthur Leyva <99632176+artleyvaGitHub@users.noreply.github.com>
2022-06-28 10:18:07 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// author : Arthur Leyva
+Console.WriteLine("Coding Challenge - Your Name Is Mine\n");
+
+Console.Write("Enter first name : ");
+string? name1 = Convert.ToString(Console.ReadLine());
+Console.Write("Enter seconde name : ");
+string? name2 = Convert.ToString(Console.ReadLine());
+    string lName = "";
+    string sName = "";
+    if(name1.Length < name2.Length) {
+        lName = name2;
+        sName = name1;
+    }
+    else {
+        lName = name1;
+        sName = name2;
+    }
+
+    if(name1.Length == name2.Length && name1 != name2) {
+        Console.WriteLine("NO! " + name1 + " & " + name2 + " are not allowed to marry");
+    }
+    else {
+        for(int i = 0; i < sName.Length; i++)
+        {
+            if(sName[i] != lName[i])
+            {
+                lName = lName.Substring(0,i) + lName.Substring(i + 1, lName.Length - i - 1);
+            }
+        }
+        lName = lName.Substring(0, sName.Length);
+        Console.WriteLine("YES! " + name1 + " & " + name2 + " are allowed to marry");
+    }
+    
+
